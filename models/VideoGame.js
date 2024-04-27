bf0c3b86d0bd58411e4e89fd10dfe3be0dccade7
malikmai <malikcreese@gmail.com>
2024-04-27 12:03:50 -0400

    Added Edit/Delete/Update functionality

--- models/VideoGame.js
+++ models/VideoGame.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const videoGameSchema = new mongoose.Schema({
   studio: { type: String, required: true },
   title: { type: String, required: true },
   genre: String,
-  year: Number
+  year: Number,
 });
 
-const VideoGame = mongoose.model('VideoGame', videoGameSchema);
+const VideoGame = mongoose.model("VideoGame", videoGameSchema);
 module.exports = VideoGame;
--- server.js
+++ server.js
@@ -11,15 +11,15 @@ mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-
 app.get("/", async (req, res) => {
   const games = await VideoGame.find({});
-  res.render("index.ejs", {games});
+  res.render("index.ejs", { games });
 });
+
 app.get("/games", async (req, res) => {
   const allGames = await VideoGame.find();
   console.log(allGames);
-  res.render("index.ejs", {games: allGames });
+  res.render("index.ejs", { games: allGames });
 });
 
 app.get("/games/new", (req, res) => {
@@ -41,11 +41,52 @@ app.get("/games/:gameId", async (req, res) => {
   }
 });
 
+app.get("/games/:gameId/edit", async (req, res) => {
+  try {
+    const foundGame = await VideoGame.findById(req.params.gameId);
+    if (!foundGame) {
+      console.log("Game not found");
+      return res.status(404).send("Game not found");
+    }
+    res.render("games/edit.ejs", { game: foundGame });
+  } catch (error) {
+    console.error("Error fetching game for editing:", error);
+    res.status(500).send("Error fetching game for editing");
+  }
+});
+
 app.post("/games", async (req, res) => {
   const { studio, title, genre, year } = req.body;
   const newGame = new VideoGame({ studio, title, genre, year });
   await newGame.save();
   res.redirect("/games");
+});
+
+app.post("/games/:gameId/delete", async (req, res) => {
+  try {
+    await VideoGame.findByIdAndDelete(req.params.gameId);
+    res.redirect("/games");
+  } catch (error) {
+    console.error("Error deleting game:", error);
+    res.status(500).send("Error deleting game");
+  }
+});
+
+app.post("/games/:gameId/edit", async (req, res) => {
+  try {
+    const { studio, title, genre, year } = req.body;
+    const updatedGame = {
+      studio,
+      title,
+      genre,
+      year
+    };
+    await VideoGame.findByIdAndUpdate(req.params.gameId, updatedGame);
+    res.redirect("/games/" + req.params.gameId);
+  } catch (error) {
+    console.error("Error updating game:", error);
+    res.status(500).send("Error updating game");
+  }
 });
 
 app.listen(3000, () => {
